f1972f9959d67a5a0a9863beed06b708832d95b7
Anai Yusary Araya Rocha <anai.yusary@gmail.com>
2023-01-28 17:32:48 +1100

    update readme

--- siteConfig.js
+++ siteConfig.js
@@ -1,9 +1,10 @@
 module.exports = {
-  name: 'London',
-  shortName: 'London',
-  description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
-  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
-  prefix: '/',
-  author: 'Imed Adel',
-  twitter: 'Imed_Adel'
+  name: "London",
+  shortName: "London",
+  description:
+    "...Failure does not mean GAME OVER, it means try again with EXPERIENCE",
+  url: "https://gatsby-london.netlify.com", // Domain of your site without prefix!
+  prefix: "/",
+  author: "Anai Araya",
+  twitter: "anaizing",
 }
