f55f49346eed8013da2e9bab80be6ab92117d5a9
Anai Yusary Araya Rocha <anai.yusary@gmail.com>
2023-01-28 17:51:35 +1100

    update config title

--- siteConfig.js
+++ siteConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-  name: "London",
-  shortName: "London",
+  name: "Anai's Portfolio",
+  shortName: "Anai's Portfolio",
   description:
     "...Failure does not mean GAME OVER, it means try again with EXPERIENCE",
   url: "https://gatsby-london.netlify.com", // Domain of your site without prefix!
   prefix: "/",
   author: "Anai Araya",
-  twitter: "anaizing",
-}
+  twitter: "anaizing"
+};
