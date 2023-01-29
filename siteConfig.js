f0e39fe110673f06742a75759e806c1ed62a0535
Anai Yusary Araya Rocha <anai.yusary@gmail.com>
2023-01-29 18:25:19 +1100

    add intro

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "Anai's Portfolio",
   shortName: "Anai's Portfolio",
   description:
-    "...Failure does not mean GAME OVER, it means try again with EXPERIENCE",
+    "I AM A FRONTEND ENGINEER, VR/XR DEVELOPER AND FOUNDER BASED IN SYDNEY. I HAVE MANY YEARS OF EXPERIENCE IN SOFTWARE DEVELOPMENT AND ALL THINGS DIGITAL. I LOVE 3D DESIGN. I LOVE GAME DEVELOPMENT. I LOVE LEARNING NEW TECH. I LOVE DANCING AND MAKING STUFF. ",
   url: "https://gatsby-london.netlify.com", // Domain of your site without prefix!
   prefix: "/",
   author: "Anai Araya",
