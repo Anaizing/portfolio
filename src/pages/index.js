ee139998cc54dc990ac27b27840f2939d2163c03
Anai Yusary Araya Rocha <anai.yusary@gmail.com>
2023-01-30 06:04:25 +1100

    add note

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ const BlogIndex = ({ data }, location) => {
           </h2>
         </header>
       )}
-      <div>webgl goes here</div>
+      <div>{/* todo: add webgl here */}</div>
       <div className="post-feed">
         {posts.map(({ node }) => {
           postCounter++;
