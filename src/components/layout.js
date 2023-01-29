b0b8a2ad644a157c833b328565fefb0be79694a5
Anai Yusary Araya Rocha <anai.yusary@gmail.com>
2023-01-29 21:33:13 +1100

    fix syntax error

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,7 +29,7 @@ const Layout = props => {
               </div>
             </div>
           </a>
-          <nav id="swup" class="site-head-left">
+          <nav id="swup" className="site-head-left">
             <ul className="nav" role="menu">
               <li className="nav-home nav-current" role="menuitem">
                 <Link to={`/`}>Projects</Link>
