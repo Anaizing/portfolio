114f9cd17f122922d516f4397e04ed7ab317351d
Anai Yusary Araya Rocha <anai.yusary@gmail.com>
2023-02-01 00:55:30 +1100

    add url domain to config, clean unused png

--- siteConfig.js
+++ siteConfig.js
@@ -3,7 +3,7 @@ module.exports = {
   shortName: "Anai's Portfolio",
   description:
     "I AM A FRONTEND ENGINEER, VR/XR DEVELOPER AND FOUNDER BASED IN SYDNEY. I HAVE MANY YEARS OF EXPERIENCE IN SOFTWARE DEVELOPMENT AND ALL THINGS DIGITAL. I LOVE 3D DESIGN. I LOVE GAME DEVELOPMENT. I LOVE LEARNING NEW TECH. I LOVE DANCING AND MAKING STUFF. ",
-  url: "https://gatsby-london.netlify.com", // Domain of your site without prefix!
+  url: "https://anai.netlify.app", // Domain of your site without prefix!
   prefix: "/",
   author: "Anai Araya",
   twitter: "anaizing"
