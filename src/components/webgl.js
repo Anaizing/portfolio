88ad7e013b362f17f11391d437cd01c31d0130e3
Anai Yusary Araya Rocha <anai.yusary@gmail.com>
2023-10-06 15:53:24 +1100

    update sample scene static files, add proto-lab static files, refactor WebGl renderer component to display proto-lab, add id to component

--- src/components/webgl.js
+++ src/components/webgl.js
@@ -2,14 +2,25 @@ import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 export const WebGLRenderer = () => {
+  // const { unityProvider } = useUnityContext({
+  //     loaderUrl: "./Build/Sample/Builds.loader.js",
+  //     dataUrl: "./Build/Sample/webgl.data",
+  //     frameworkUrl: "./Build/Sample/build.framework.js",
+  //     codeUrl: "./Build/Sample/build.wasm"
+  // });
+
   const { unityProvider } = useUnityContext({
-    loaderUrl: "./Sample/Build/Builds.loader.js",
-    dataUrl: "./Sample/Build/webgl.data",
-    frameworkUrl: "./Sample/Build/build.framework.js",
-    codeUrl: "./Sample/Build/build.wasm"
+    loaderUrl: "./Build/Proto-Lab/portfolio-scenes.loader.js",
+    dataUrl: "./Build/Proto-Lab/webgl.data",
+    frameworkUrl: "./Build/Proto-Lab/build.framework.js",
+    codeUrl: "./Build/Proto-Lab/build.wasm"
   });
 
   return (
-    <Unity unityProvider={unityProvider} style={{ width: 400, height: 600 }} />
+    <Unity
+      id="react-unity-webgl-canvas-2"
+      unityProvider={unityProvider}
+      style={{ width: 800, height: 400 }}
+    />
   );
 };
