bf1ef98d708b0f809dc088bfe04967318a65709c
Anai Yusary Araya Rocha <anai.yusary@gmail.com>
2023-10-24 08:51:13 +1100

    remove unused code to other game scene

--- src/components/webgl.js
+++ src/components/webgl.js
@@ -2,13 +2,6 @@ import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 export const WebGLRenderer = () => {
-  // const { unityProvider } = useUnityContext({
-  //     loaderUrl: "./Build/Sample/Builds.loader.js",
-  //     dataUrl: "./Build/Sample/webgl.data",
-  //     frameworkUrl: "./Build/Sample/build.framework.js",
-  //     codeUrl: "./Build/Sample/build.wasm"
-  // });
-
   const { unityProvider } = useUnityContext({
     loaderUrl: "./Build/Proto-Lab/portfolio-scenes.loader.js",
     dataUrl: "./Build/Proto-Lab/webgl.data",
